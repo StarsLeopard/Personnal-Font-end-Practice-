4e3f140deffa6fce9cea216db794baedcd97f2a6
StarsLeopard <1240056120@qq.com>
2021-07-30 10:00:04 +0800

    删除了注释部分，用以进行git仓库管理测试

--- 01.protect1/01.js
+++ 01.protect1/01.js
@@ -2,12 +2,7 @@ function auto(inter) {
     inter ++;
     inter %= 3;
     $(".swiper-container").css("transform","translateY("+ (-240 * inter)+"px)");
-    // if(inter == 3){
-    //     $(".swiper-container").toggleClass("no-transition");
-    //     $(".no-transition").css("transform","translateY(0px)");
-    //     $(".no-transition").toggleClass("swiper-container");
-    //     inter = 0;
-    // }
+
     $(".pagination .swiper-pagination-switch").removeClass("swiper-visible-switch");
     $(".swiper-pagination-switch").eq(inter).addClass("swiper-visible-switch");
     return inter;
